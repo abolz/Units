3525b13b04433ce66460c924deb835ed3b68d8e4
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2021-03-14 12:23:38 +0100

    Add copyright notice

--- src/Unit.h
+++ src/Unit.h
@@ -1,3 +1,8 @@
+// Copyright 2021 Alexander Bolz
+//
+// Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <cassert>
