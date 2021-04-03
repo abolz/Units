592166ea6920f334cc1908c80e4b5bc9a0987bd4
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2021-04-03 12:29:08 +0200

    Clean up example

--- test/Xkcd.cc
+++ test/Xkcd.cc
@@ -12,7 +12,9 @@ using Birds  = TaggedQuantity<Entities, struct _birds>;
 using Poops  = TaggedQuantity<Entities, struct _poops>;
 using Mouths = TaggedQuantity<Entities, struct _mouths>;
 
-inline constexpr auto k4Pi = 4 * impl::kPi;
-inline constexpr auto kEarthRadius = 6371.0_km;
+inline constexpr auto PI4 = 4 * 3.1415;
+inline constexpr auto R = 6371_km;
+inline constexpr auto Rsq = R * R;
 
-inline constexpr Years answer(1 / ((Birds(300.0e+9) / (k4Pi * (kEarthRadius * kEarthRadius))) * ((Poops(1) / Birds(1)) / 1_h) * (16_h / 1_d) * (Mouths(1) / Poops(1)) * (15_cm2 / Mouths(1))));
+inline constexpr Years answer
+    = Years(1 / ((Birds(300.0e+9) / (PI4 * Rsq)) * ((Poops(1) / Birds(1)) / 1_h) * (16_h / 1_d) * (Mouths(1) / Poops(1)) * (15_cm2 / Mouths(1))));
