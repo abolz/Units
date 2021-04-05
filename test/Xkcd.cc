913dad48744da7fe679500cdd5460b6e7a2cfd30
Alexander Bolz <lt.morris.schaffer@googlemail.com>
2021-04-05 14:12:56 +0200

    Slightly "clean up" example

--- test/Xkcd.cc
+++ test/Xkcd.cc
@@ -12,9 +12,28 @@ using Birds  = TaggedQuantity<Entities, struct _birds>;
 using Poops  = TaggedQuantity<Entities, struct _poops>;
 using Mouths = TaggedQuantity<Entities, struct _mouths>;
 
-inline constexpr auto PI4 = 4 * 3.1415;
-inline constexpr auto R = 6371_km;
-inline constexpr auto Rsq = R * R;
+using Gigabirds = ScaledQuantity<Conversion<Ratio<1'000'000'000>>, Birds>;
 
-inline constexpr Years answer
-    = Years(1 / ((Birds(300.0e+9) / (PI4 * Rsq)) * ((Poops(1) / Birds(1)) / 1_h) * (16_h / 1_d) * (Mouths(1) / Poops(1)) * (15_cm2 / Mouths(1))));
+static constexpr auto PI4 = 4 * 3.1415;
+static constexpr auto R = 6371_km;
+static constexpr auto Rsq = R * R;
+
+static constexpr Years answer
+    = Years(
+        1 / ((Gigabirds(300.0) / (PI4 * Rsq))
+                * ((Poops(1_ent) / Birds(1_ent)) / 1_h)
+                * (16_h / 1_d)
+                * (Mouths(1_ent) / Poops(1_ent))
+                * (15_cm2 / Mouths(1_ent))));
+
+static_assert(answer.count_internal() == 193.9538756997824294);
+//static_assert(constexpr_math::ieee754_round(answer.count_internal()) == 194);
+//static_assert(round(answer).count_internal() == 194);
+
+//int main()
+//{
+//    const auto y1 = round<Ratio<5>>(answer); // round to 5-years
+//    const auto y2 = round<Weeks>(answer);
+//    const auto y3 = round(Weeks(answer));
+//    return 0;
+//}
